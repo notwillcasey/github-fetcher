add6708c21677231ac4a0f0411fc3d5831402dfc
notwillcasey <notwillcasey@gmail.com>
2024-02-11 01:05:04 -0700

    update styling for user component

--- src/app/interfaces/user.interface.ts
+++ src/app/interfaces/user.interface.ts
@@ -2,7 +2,7 @@ export interface User {
     login?: string;
     id?: number;   
     nodeId?: string;
-    avatar_url?: string;
+    avatar_Url?: string;
     gravatarId?: string;
     url?: string;
     htmlUrl?: string;
